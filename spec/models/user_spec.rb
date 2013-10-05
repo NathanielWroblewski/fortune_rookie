be161281918a3bd97ab4a350ebee69de868ac428
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2013-10-05 14:01:53 -0700

    added balance to users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
-describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User, '#balance_as_money' do
+  it 'shows the balance as a dollar amount' do
+    user = create(:user, balance: 100_00)
+
+    expect(user.balance_as_money).to eq '$100.00'
+  end
 end
