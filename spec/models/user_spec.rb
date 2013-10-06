50eb98867215d799d8c37fa2eff5233b305dd61f
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2013-10-05 17:02:35 -0700

    added holdings to user show

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
+describe User, 'associations' do
+  it { expect(subject).to have_many(:holdings) }
+end
+
 describe User, '#balance_as_money' do
   it 'shows the balance as a dollar amount' do
     user = create(:user, balance: 100_00)
